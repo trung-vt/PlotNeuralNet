fdd05b57a91e50944da04bebc8df360ee0cc2e60
trung-vt <trung-vt>
2024-06-14 01:32:22 +0000

    upd unet

--- unet-true-voice-32/unet-visualisation-true-voice-32.py
+++ unet-true-voice-32/unet-visualisation-true-voice-32.py
@@ -10,31 +10,79 @@ arch = [
     to_begin(),
     
     #input
-    to_input( '../examples/fcn8s/cats.jpg' ),
+    # to_input( '../examples/fcn8s/cats.jpg', width=8, height=8),
+    to_input( '../examples/noisy.png', width=8, height=8),
 
-
-
-
-    #block-000: double conv only 1->32
-    to_ConvConvRelu( 
-        name='ccr_b1', ###############
+    to_Conv(
+        name='ccr_b1a', ###############
         s_filer=256, 
         n_filer=(32,32), 
-        offset="(0,0,0)", to="(0,0,0)", 
-        width=(2,2), height=40, depth=40  ),
+        offset="(0,0,0)", 
+        to="(0,0,0)", 
+        width=2, 
+        height=40, depth=40  ),
+
+
+    to_Conv(
+        name='ccr_b1b', ###############
+        s_filer=256, 
+        n_filer=(32,32), 
+        offset="(1,0,0)", 
+        to="(ccr_b1a-east)", 
+        width=2, 
+        height=40, depth=40  ),
+    
+
+    # #block-000: double conv only 1->32
+    # to_ConvConvRelu( 
+    #     name='ccr_b1', ###############
+    #     s_filer=256, 
+    #     n_filer=(32,32), 
+    #     offset="(0,0,0)", 
+    #     # to="(0,0,0)",
+    #     # to="(ccr_b1a-east)",
+    #     to="(ccr_b1b-east)",
+    #     width=(2,2), 
+    #     height=40, depth=40  ),
+
+
+    # to_Conv(
+    #     name='ccr_b1a', ###############
+    #     s_filer=256, 
+    #     n_filer=(32,32), 
+    #     offset="(0,0,0)", 
+    #     to="(0,0,0)", 
+    #     width=2, 
+    #     height=40, depth=40  ),
+
+    # to_Conv(
+    #     name='ccr_b1b', ###############
+    #     s_filer=256, 
+    #     n_filer=(32,32), 
+    #     offset="(1,0,0)", 
+    #     to="(ccr_b1a-east)", 
+    #     width=2, 
+    #     height=40, depth=40  ),
 
     # #arrow
-    # to_connection( "temp", "ccr_b1"),
+    # to_connection( "(0, 0, 0)", "ccr_b1"),
 
     #block-001: maxpool + double conv 32->64
     to_Pool(
         name="pool_b1", ##############
-        offset="(1,0,0)", 
-        to="(ccr_b1-east)", #################
-        width=1, height=32, depth=32, opacity=0.5),
+        # offset="(-0.6,-8,-0.6)", 
+        offset="(0, -5, 0)", 
+        # to="(ccr_b1-east)", #################
+        # to="(ccr_b1a-east)", #################
+        to="(ccr_b1b-southwest)", #################
+        width=2, 
+        height=20, depth=20, opacity=0.5),
     
-    #arrow
-    to_connection( "ccr_b1", "pool_b1"),
+    # #arrow
+    # to_connection( "ccr_b1", "pool_b1"),
+    # to_connection( "ccr_b1a", "pool_b1"),
+    # to_connection( "ccr_b1b", "pool_b1"),
+    to_connection_vertical( "ccr_b1b", "pool_b1"),
 
     # *block_2ConvPool( 
     #     name='b2', ##############
@@ -51,29 +99,35 @@ arch = [
         n_filer=(64,64), 
         offset="(0,0,0)", 
         to="(pool_b1-east)", 
-        width=(3.5, 3.5), 
-        height=32, 
-        depth=32,   
+        width=(4, 4), 
+        height=20, 
+        depth=20,   
         ),    
+
+    # #arrow
+    # to_connection_vertical( 
+    #     "ccr_b1", 
+    #     "ccr_b2"
+    # ),
 
     #block-002: maxpool + double conv 64->128
     to_Pool(         
         name="pool_b2", 
-        offset="(1,0,0)", 
+        offset="(0,-4.5,0)", 
         to="(ccr_b2-east)",  
-        width=1,         
-        height=32 - 8, 
-        depth=32 - 8, 
+        width=4,         
+        height=10, 
+        depth=10, 
         opacity=0.5, 
         ),
 
-    # arrow
-    to_connection( 
-        # "pool_b1", 
-        "ccr_b2",
-        "pool_b2"
-        ),
-    # ],
+    # # arrow
+    # to_connection( 
+    #     # "pool_b1", 
+    #     "ccr_b2",
+    #     "pool_b2"
+    #     ),
+    # # ],
 
     # *block_2ConvPool( 
     #     name='b3', ##############
@@ -91,11 +145,21 @@ arch = [
         s_filer=str(64), 
         n_filer=(128,128), 
         offset="(0,0,0)", 
+        # to="(pool_b2-east)", 
         to="(pool_b2-east)", 
-        width=(3.5, 3.5), 
-        height=32 - 8, 
-        depth=32 - 8,   
+        width=(12, 12), 
+        height=10, 
+        depth=10,   
         ),    
+
+    # arrow
+    to_connection( 
+        # "pool_b1", 
+        "ccr_b2",
+        # "pool_b2"
+        "ccr_b3"
+        ),
+    # ],
 
     #block-003: maxpool + double conv 128->256
     to_Pool(         
@@ -103,8 +167,8 @@ arch = [
         offset="(1,0,0)", 
         to="(ccr_b3-east)",  
         width=1,         
-        height=32 - 16, 
-        depth=32 - 16, 
+        height=5, 
+        depth=5, 
         opacity=0.5, 
         ),
 
@@ -131,153 +195,346 @@ arch = [
         n_filer=(256,256), 
         offset="(0,0,0)", 
         to="(pool_b3-east)", 
-        width=(3.5, 3.5), 
-        height=32 - 16, 
-        depth=32 - 16,   
+        width=(18, 18), 
+        height=5, 
+        depth=5,   
         ),    
 
-    #block-003: maxpool + double conv 128->256
-    to_Pool(         
-        name="pool_b4", 
-        offset="(1,0,0)", 
-        to="(ccr_b4-east)",  
-        width=1,         
-        height=32 - 24, 
-        depth=32 - 24, 
-        opacity=0.5, 
-        ),
+    # #block-003: maxpool + double conv 128->256
+    # to_Pool(         
+    #     name="pool_b4", 
+    #     offset="(0.5,0,0)", 
+    #     to="(ccr_b4-east)",  
+    #     width=1,         
+    #     height=32 - 24, 
+    #     depth=32 - 24, 
+    #     opacity=0.5, 
+    #     ),
 
-    # arrow
-    to_connection( 
-        # "pool_b3"
-        "ccr_b4",
-        "pool_b4"
-        ),
-    # ],
-
-
-    #Bottleneck
-    #block-005
-    to_ConvConvRelu( 
-        name='ccr_b5', 
-        s_filer=16, 
-        n_filer=(512,512), 
-        offset="(0,0,0)", 
-        to="(pool_b4-east)", ##############
-        width=(8,8), height=8, depth=8, caption="Bottleneck"  ),
-
-    # to_connection( "pool_b4", "ccr_b5"),
+    # # arrow
+    # to_connection( 
+    #     # "pool_b3"
+    #     "ccr_b4",
+    #     "pool_b4"
+    #     ),
+    # # ],
 
 
+    # #Bottleneck
+    # #block-005
+    # to_ConvConvRelu( 
+    #     name='ccr_b5', 
+    #     s_filer=16, 
+    #     n_filer=(512,512), 
+    #     offset="(0,0,0)", 
+    #     to="(pool_b4-east)", ##############
+    #     width=(8,8), height=8, depth=8, caption="Bottleneck"  ),
 
-    #Decoder
-    *block_Unconv( 
-        name="b6", 
-        botton="ccr_b5", 
-        top='end_b6', 
-        s_filer=32,  
-        n_filer=256, 
-        offset="(1,0,0)", size=(16,16,5.0), opacity=0.5 ),
+    # # to_connection( "pool_b4", "ccr_b5"),
+
+
+
+    # #Decoder
+    # *block_Unconv( 
+    #     name="b6", 
+    #     botton="ccr_b5", 
+    #     top='end_b6', 
+    #     s_filer=32,  
+    #     n_filer=256, 
+    #     offset="(1,0,0)", size=(16,16,5.0), opacity=0.5 ),
 
     # to_skip( of='ccr_b4', to='ccr_res_b6', pos=1.25),
 
-    # # [
+    # # # [
     #     to_UnPool(  
-    #         name='unpool_{}'.format(name),    
-    #         offset=offset,    
-    #         to="({}-east)".format(botton),         
+    #         name='unpool_b6',    
+    #         offset="(1,0,0)",    
+    #         # to="(ccr_b5-east)",         
+    #         to="(ccr_b4-east)",         
     #         width=1,              
-    #         height=size[0],       
-    #         depth=size[1], 
-    #         opacity=opacity 
+    #         height=16,       
+    #         depth=16, 
+    #         opacity=0.5 
     #         ),
     #     to_ConvRes( 
-    #         name='ccr_res_{}'.format(name),   
-    #         offset="(0,0,0)", to="(unpool_{}-east)".format(name),    
-    #         s_filer=str(s_filer), 
-    #         n_filer=str(n_filer), 
-    #         width=size[2], 
-    #         height=size[0], 
-    #         depth=size[1], 
-    #         opacity=opacity 
+    #         name='ccr_res_b6',   
+    #         offset="(0,0,0)", 
+    #         to="(unpool_b6-east)",    
+    #         s_filer=str(32), 
+    #         n_filer=str(256), 
+    #         width=5.0, 
+    #         height=16, 
+    #         depth=16, 
+    #         opacity=0.5 
     #         ),       
     #     to_Conv(    
-    #         name='ccr_{}'.format(name),       
+    #         name='ccr_b6',       
     #         offset="(0,0,0)", 
-    #         to="(ccr_res_{}-east)".format(name),   
-    #         s_filer=str(s_filer), 
-    #         n_filer=str(n_filer), 
-    #         width=size[2], 
-    #         height=size[0], 
-    #         depth=size[1] 
+    #         to="(ccr_res_b6-east)",   
+    #         s_filer=str(32), 
+    #         n_filer=str(256), 
+    #         width=5.0, 
+    #         height=16, 
+    #         depth=16
     #         ),
     #     to_ConvRes( 
-    #         name='ccr_res_c_{}'.format(name), 
+    #         name='ccr_res_c_b6', 
     #         offset="(0,0,0)", 
-    #         to="(ccr_{}-east)".format(name),       
-    #         s_filer=str(s_filer), 
-    #         n_filer=str(n_filer), 
-    #         width=size[2], 
-    #         height=size[0], 
-    #         depth=size[1], 
-    #         opacity=opacity 
+    #         to="(ccr_b6-east)",       
+    #         s_filer=str(32), 
+    #         n_filer=str(256), 
+    #         width=5.0, 
+    #         height=16, 
+    #         depth=16,
+    #         opacity=0.5 
     #         ),       
     #     to_Conv(    
-    #         name='{}'.format(top),            
+    #         name='end_b6',            
     #         offset="(0,0,0)", 
-    #         to="(ccr_res_c_{}-east)".format(name), 
-    #         s_filer=str(s_filer), 
-    #         n_filer=str(n_filer), 
-    #         width=size[2], 
-    #         height=size[0], 
-    #         depth=size[1] 
+    #         to="(ccr_res_c_b6-east)", 
+    #         s_filer=str(32), 
+    #         n_filer=str(256), 
+    #         width=5.0, 
+    #         height=16, 
+    #         depth=16
     #         ),
     #     to_connection( 
-    #         "{}".format( botton ), 
-    #         "unpool_{}".format( name ) 
+    #         # "ccr_b5", 
+    #         "ccr_b4", 
+    #         "unpool_b6"
     #         ),
+    # # # ]
+
+
+    # *block_Unconv( 
+    #     name="b7", 
+    #     botton="end_b6", 
+    #     top='end_b7', 
+    #     s_filer=64, 
+    #     n_filer=128, 
+    #     offset="(2.1,0,0)", size=(25,25,4.5), opacity=0.5 ),
+
+    # # [
+        to_UnPool(  
+            name='unpool_b7',    
+            offset="(1,0,0)",    
+            # to="(ccr_b6-east)",         
+            to="(ccr_b4-east)",         
+            # to="(end_b6-east)",         
+            width=1,              
+            height=10,       
+            depth=10, 
+            opacity=0.5 
+            ),
+        to_ConvRes( 
+            name='ccr_res_b7',   
+            offset="(0,0,0)", 
+            to="(unpool_b7-east)",    
+            s_filer=str(64), 
+            n_filer=str(128), 
+            width=6, 
+            height=10, 
+            depth=10, 
+            opacity=0.5 
+            ),       
+        to_Conv(    
+            name='ccr_b7',       
+            offset="(0,0,0)", 
+            to="(ccr_res_b7-east)",   
+            s_filer=str(64), 
+            n_filer=str(128), 
+            width=6, 
+            height=10, 
+            depth=10
+            ),
+        to_ConvRes( 
+            name='ccr_res_c_b7', 
+            offset="(0,0,0)", 
+            to="(ccr_b7-east)",       
+            s_filer=str(64), 
+            n_filer=str(128), 
+            width=6, 
+            height=10, 
+            depth=10,
+            opacity=0.5 
+            ),       
+        to_Conv(    
+            name='end_b7',            
+            offset="(0,0,0)", 
+            to="(ccr_res_c_b7-east)", 
+            s_filer=str(64), 
+            n_filer=str(128), 
+            width=6, 
+            height=10, 
+            depth=10
+            ),
+        to_connection( 
+            # "ccr_b6",
+            "ccr_b4",
+            # "end_b6", 
+            "unpool_b7"
+            ),
     # # ]
 
 
-    *block_Unconv( 
-        name="b7", 
-        botton="end_b6", 
-        top='end_b7', 
-        s_filer=64, 
-        n_filer=128, 
-        offset="(2.1,0,0)", size=(25,25,4.5), opacity=0.5 ),
-
     to_skip( of='ccr_b3', to='ccr_res_b7', pos=1.25),  
 
-    *block_Unconv( 
-        name="b8",
-          botton="end_b7", 
-          top='end_b8', 
-          s_filer=128, 
-          n_filer=64, 
-          offset="(2.1,0,0)", size=(32,32,3.5), opacity=0.5 ),
+
+    # *block_Unconv( 
+    #     name="b8",
+    #       botton="end_b7", 
+    #       top='end_b8', 
+    #       s_filer=128, 
+    #       n_filer=64, 
+    #       offset="(2.1,0,0)", size=(32,32,3.5), opacity=0.5 ),
+
+    # # [
+        to_UnPool(  
+            name='unpool_b8',    
+            offset="(0,4.5,0)",    
+            # to="(ccr_b7-east)",         
+            to="(end_b7-east)",         
+            width=1,              
+            height=20,       
+            depth=20, 
+            opacity=0.5 
+            ),
+        to_ConvRes( 
+            name='ccr_res_b8',   
+            offset="(0,0,0)", 
+            to="(unpool_b8-east)",    
+            s_filer=str(128), 
+            n_filer=str(64), 
+            width=5, 
+            height=20, 
+            depth=20, 
+            opacity=0.5 
+            ),       
+        to_Conv(    
+            name='ccr_b8',       
+            offset="(0,0,0)", 
+            to="(ccr_res_b8-east)",   
+            s_filer=str(128), 
+            n_filer=str(64), 
+            width=5, 
+            height=20, 
+            depth=20
+            ),
+        to_ConvRes( 
+            name='ccr_res_c_b8', 
+            offset="(0,0,0)", 
+            to="(ccr_b8-east)",       
+            s_filer=str(128), 
+            n_filer=str(64), 
+            width=5, 
+            height=20, 
+            depth=20,
+            opacity=0.5 
+            ),       
+        to_Conv(    
+            name='end_b8',            
+            offset="(0,0,0)", 
+            to="(ccr_res_c_b8-east)", 
+            s_filer=str(128), 
+            n_filer=str(64), 
+            width=5, 
+            height=20, 
+            depth=20
+            ),
+        to_connection( 
+            # "ccr_b7",
+            "end_b7", 
+            "unpool_b8"
+            ),
+    # # ]
 
     to_skip( of='ccr_b2', to='ccr_res_b8', pos=1.25),    
     
 
 
-    *block_Unconv( 
-        name="b9", 
-        botton="end_b8", 
-        top='end_b9', 
-        s_filer=256,
-        n_filer=32,  
-        offset="(2.1,0,0)", size=(40,40,2.5), opacity=0.5 ),
+    # *block_Unconv( 
+    #     name="b9", 
+    #     botton="end_b8", 
+    #     top='end_b9', 
+    #     s_filer=256,
+    #     n_filer=32,  
+    #     offset="(2.1,0,0)", size=(40,40,2.5), opacity=0.5 ),
 
-    to_skip( of='ccr_b1', to='ccr_res_b9', pos=1.25),
+    # # [
+        to_UnPool(  
+            name='unpool_b9',    
+            offset="(0,8,0)",    
+            # to="(ccr_b8-east)",         
+            to="(end_b8-east)",         
+            width=1,              
+            height=16 + 24,       
+            depth=16 + 24, 
+            opacity=0.5 
+            ),
+        to_ConvRes( 
+            name='ccr_res_b9',   
+            offset="(0,0,0)", 
+            to="(unpool_b9-east)",    
+            s_filer=str(256), 
+            n_filer=str(32), 
+            width=3, 
+            height=16 + 24, 
+            depth=16 + 24, 
+            opacity=0.5 
+            ),       
+        to_Conv(    
+            name='ccr_b9',       
+            offset="(0,0,0)", 
+            to="(ccr_res_b9-east)",   
+            s_filer=str(256), 
+            n_filer=str(32), 
+            width=3, 
+            height=16 + 24, 
+            depth=16 + 24
+            ),
+        to_ConvRes( 
+            name='ccr_res_c_b9', 
+            offset="(0,0,0)", 
+            to="(ccr_b9-east)",       
+            s_filer=str(256), 
+            n_filer=str(32), 
+            width=3, 
+            height=16 + 24, 
+            depth=16 + 24,
+            opacity=0.5 
+            ),       
+        to_Conv(    
+            name='end_b9',            
+            offset="(0,0,0)", 
+            to="(ccr_res_c_b9-east)", 
+            s_filer=str(256), 
+            n_filer=str(32), 
+            width=3, 
+            height=16 + 24, 
+            depth=16 + 24
+            ),
+        to_connection( 
+            # "ccr_b8",
+            "end_b8", 
+            "unpool_b9"
+            ),
+    # # ]
+
+    to_skip( of='ccr_b1b', to='ccr_res_b9', pos=1.25),
     
     to_ConvSoftMax( 
         name="soft1", 
         s_filer=256, 
         offset="(0.75,0,0)", 
         to="(end_b9-east)",
-        width=1, height=40, depth=40, caption="SOFT" ),
+        width=1, height=40, depth=40, 
+        # caption="SOFT" 
+        # caption="MSE"
+        ),
+
     to_connection( "end_b9", "soft1"),
+
+    # to_input( '../examples/fcn8s/cats.jpg', width=8, height=8),
      
     to_end() 
     ]
